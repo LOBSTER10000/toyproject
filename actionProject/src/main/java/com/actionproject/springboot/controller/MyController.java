fc978ca4297f841b39569b887e6ca65528aebdf9
LOBSTER10000 <111476138+LOBSTER10000@users.noreply.github.com>
2023-03-09 18:10:02 +0900

    오늘자 커밋

--- actionProject/src/main/java/com/actionproject/springboot/controller/MyController.java
+++ actionProject/src/main/java/com/actionproject/springboot/controller/MyController.java
@@ -13,20 +13,41 @@ public class MyController {
     }
 
     @GetMapping("/login")
-    public String go2(){return "login";}
+    public String login(){return "login";}
 
     @GetMapping("/join")
-    public String go3(){return "join";};
+    public String join(){return "join";};
 
     @GetMapping("/notice")
-    public String go4(){return "notice";}
+    public String notice(){return "notice";}
 
     @GetMapping("/qna")
-    public String go5(){return "qna";}
+    public String qna(){return "qna";}
 
     @GetMapping("/view")
-    public String go6(){return "view";}
+    public String view(){return "view";}
 
     @GetMapping("/myPage")
-    public String go7(){return "/myPage";}
+    public String myPage(){return "/myPage";}
+
+    @GetMapping("/registration")
+    public String registration(){return "/registration";}
+
+    @GetMapping("/modify")
+    public String modify(){return "/modify";}
+
+    @GetMapping("/bbs")
+    public String bbs(){return "/bbs";}
+
+    @GetMapping("/cart")
+    public String cart(){return "/cart";}
+
+    @GetMapping("/write")
+    public String add(){return "/write";}
+
+    @GetMapping("/update")
+    public String update(){return "/update";}
+
+    @GetMapping("/mainView")
+    public String mainView(){return "/mainView";}
 }
